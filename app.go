6f67164d9ea3547411aa1974c753d55c78e4e26d
chenzhangchao <chenzhangchao@cai-inc.com>
2022-03-22 11:56:04 +0800

    feat: 上传项目

---
+++ app.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"name":     "hedui",
+			"age":      23,
+			"describe": "from FuJian Province",
+			"hobby":    "simple is better",
+		})
+	})
+	r.Run(":8085")
+}
